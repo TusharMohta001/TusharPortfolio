3acbc7aae27fb550a29438c3ade419a2c50328a1
User@Tushar <user.tushar>
2025-12-05 13:55:10 +0530

    Sync local changes

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,23 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+
+// const HeavyComponent = React.lazy(() => import('./HeavyComponent'));
+
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   server:{
     host:true
   },
    build: {
-    // Increase the warning limit to 2 MB
-    chunkSizeWarningLimit: 2000
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
   }
 })
